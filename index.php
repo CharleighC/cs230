bb7e93e13a3a3fb9a97f0043e5702c2d8eaf23ae
CharleighC <cac0068@mix.wvu.edu>
2021-02-05 19:21:29 -0500

    Lab2: about page

---
+++ index.php
@@ -0,0 +1,54 @@
+<body>
+    <link rel="stylesheet" href="css/about.css">
+
+    <div class="about">
+        <h1> Welcome!</h1>
+        <p> Lets judge some TV shows. The rating team will be myself and google.    </p>
+    </div>
+    <div class="row">
+        <div class="column">
+            <div class ="card">
+                <img src="images/6.png">
+                <div class="container">
+                    <h2>Netflix</h2>
+                    <p> Google </p>
+                    <p>Email: 1234@gmail.com</p>
+
+                </div>
+            </div>
+        </div>
+        <div class="column">
+            <div class ="card">
+                <img src="images/7.png">
+                <div class="container">
+                    <h2>Hulu</h2>
+                    <p>Google</p>
+                    <p>Email: 1234@gmail.com</p>
+
+                </div>
+            </div>
+        </div>
+        <div class="column">
+            <div class ="card">
+                <img src="images/8.png">
+                <div class="container">
+                    <h2>HBO</h2>
+                    <p>Google</p>
+                    <p>Email: 1234@gmail.com</p>
+
+                </div>
+            </div>
+        </div>
+        <div class="column">
+            <div class ="card">
+                <img src="images/9.jpeg">
+                <div class="container">
+                <h2>Prime</h2>
+                    <p> Google </p>
+                    <p>Email: 1234@gmail.com</p>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</body>

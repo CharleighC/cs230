9af156c993979815a1a0104d45d9cdbd71561be0
CharleighC <cac0068@mix.wvu.edu>
2021-02-25 02:49:31 -0500

    Lab 5

--- includes/login-helper.php
+++ includes/login-helper.php
@@ -30,15 +30,17 @@ if(isset($_POST['login-submit'])) {
             $pass_check = password_verify($passw, $data['password']);
             if($pass_check == true) {
                 session_start();
-                $_SRSSION['uid'] = $data['uid'];
+                $_SESSION['uid'] = $data['uid'];
                 $_SESSION['fname'] = $data['fname'];
                 $_SESSION['uname'] = $data['uname'];
 
-                echo "<h1>Success!</h1><p>$uname</p>";
+                //echo "<h1>Success!</h1><p>$uname</p>)";
+                header("Location: ../profile.php?success=login");
+                exit();
             }else{
                 header("Location: ../login.php?error=WrongPass");
                 exit();
-            }
+                 }
             }
         }
     
